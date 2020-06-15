3c613faff9538ddd254e9558687543255e2b43c7
digetl <dave@digetl.co.uk>
2020-06-15 16:12:55 +0100

    Subtract 1 number from another - passed

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -31,4 +31,8 @@ public class Calculator {
     public int add(int numberOne, int numberTwo){
         return numberOne + numberTwo;
     }
+
+    public int subtract(int numberOne, int numberTwo){
+        return numberOne - numberTwo;
+    }
 }

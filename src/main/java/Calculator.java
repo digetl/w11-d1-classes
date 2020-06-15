386f86a4b84e9bdb6e69d787b4ec86e697d9f84f
digetl <dave@digetl.co.uk>
2020-06-15 15:51:05 +0100

    test1 failed again

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,12 @@
 public class Calculator {
+    private int numberOne;
+    private int numberTwo;
+
+    private double numberOneDouble;
+    private double numberTwoDouble;
+
+    public Calculator(int numberOne, int numberTwo){
+        this.numberOne = numberOne;
+        this.numberTwo = numberTwo;
+    }
 }

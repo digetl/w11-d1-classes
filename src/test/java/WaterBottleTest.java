91f0ec0e46be514099e30bc18d3215b038ba6560
digetl <dave@digetl.co.uk>
2020-06-15 16:40:16 +0100

    Water bottle volume failed

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,4 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    WaterBottle waterBottle;
+    int volume = 100;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle(volume);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEqual(100, waterBottle.getVolume);
+    }
+
 
 
 }

be3d0c20a128476e6776b17b6b578b90cb3b5c69
digetl <dave@digetl.co.uk>
2020-06-15 16:03:10 +0100

    Get numberOneDouble - passed

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -23,4 +23,8 @@ public class Calculator {
     public double getNumberTwoDouble(){
         return numberTwoDouble;
     }
+
+    public double getNumberOneDouble(){
+        return numberOneDouble;
+    }
 }
